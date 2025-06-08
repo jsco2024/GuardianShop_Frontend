024b1b062d587e4a1db9aefc71242a66512824b8
Andres Moreno Pedraza <jamp9504@gmail.com>
2025-06-08 01:46:07 -0500

    se cambia i por I en la ruta import instagram from '../assets/Instagram.svg' del footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import world from '../assets/world.svg'
 import { Link } from 'react-router-dom'
 import meta from '../assets/meta.svg'
 import tiktok from '../assets/tiktok.svg'
-import instagram from '../assets/instagram.svg'
+import instagram from '../assets/Instagram.svg'
 import twitter from '../assets/twitter.svg'
 import WhatsApp from '../assets/WhatsApp.svg'
 
